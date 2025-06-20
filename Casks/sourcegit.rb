eb3db22410379539b9961d00ca014a498d101a04
ttdjong <ttdjong@outlook.com>
2025-06-20 12:00:40 +0800

    Update sourcegit.rb

--- Casks/sourcegit.rb
+++ Casks/sourcegit.rb
@@ -1,9 +1,9 @@
 cask "sourcegit" do
   arch arm: "arm64", intel: "x64"
 
-  version "2025.21"
-  sha256 arm:   "f9add67f6630ebd6d4c7524a8f1d37e4e61a39fcd7d4449e60270a9cca605400",
-         intel: "dc4301574b4558e311588bbed1486721e96ab1bb226fcad810b9391e43fc696b"
+  version "2025.22"
+  sha256 arm:   "f0d3d1cceefbd94218d8ce8f6e25df42c4a2524d2bddc24bd20c8b6adc6c5538",
+         intel: "640d858fc33b1064b37eb790abd2b4761ff9f6559e0d572cd32f95635d560734"
 
   url "https://github.com/sourcegit-scm/sourcegit/releases/download/v#{version}/sourcegit_#{version}.osx-#{arch}.zip",
       verified: "github.com/sourcegit-scm/sourcegit/"
